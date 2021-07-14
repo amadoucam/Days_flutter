23e3b4b5d8aa3b745bf12a3e86dd372dfab990e4
Camara Amadou <Camara>
2021-07-14 23:49:17 +0100

    first commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget{
+  final int days = 30;
+  final String name = " par Camara";
+  double pi = 3.14;
+  bool isMale = true;
+  num temps = 30.5;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container (
+          child: Text("Welcome flutter $days days series $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }  
+}
+
+/**
+ int days = 30;
+  String name = " par Camara";
+  double pi = 3.14;
+  bool isMale = true;
+  num temps = 30.5;
+
+  @override
+  Widget build(BuildContext context) {
+/*
+    return MaterialApp(
+      home: Material(
+        child: Center(
+          child: Container (
+          child: Text("Welcome flutter $days days series" + name),
+          ),
+        ),
+      ),
+    ); */
+  }
+
+ */
