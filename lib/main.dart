e2098b2097a743ffd702ec3ffa3de0a743e76ed0
Camara Amadou <Camara>
2021-07-16 01:27:53 +0100

    Day 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/utils/routes.dart';
 import 'pages/home_page.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'pages/login_page.dart';
+import 'utils/routes.dart';
+import 'widgets/themes.dart';
 
 void main() {
   runApp(MyApp());
 }
-//fvm git
+
 class MyApp extends StatelessWidget {
-  //const MyApp({Key key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      //home: HomePage(), //pas besoin si ya router
-      themeMode: ThemeMode.light, //light or dark
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        fontFamily: GoogleFonts.lato().fontFamily,
-        //primaryTextTheme: GoogleFonts.latoTextTheme()
-      ),
+      themeMode: ThemeMode.light,
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
       debugShowCheckedModeBanner: false,
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-        //primarySwatch: Colors.red
-      ),
-      initialRoute: "/",
+      initialRoute: MyRoutes.homeRoute,
       routes: {
         "/": (context) => LoginPage(),
         MyRoutes.homeRoute: (context) => HomePage(),
-        MyRoutes.loginRoute: (context) => LoginPage(),
+        MyRoutes.loginRoute: (context) => LoginPage()
       },
     );
   }
-
-  /*
-  bringVegetables({required bool thaila,int rupees = 100}) {
-    //
-  } */
-
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,44 +1,31 @@
 import 'package:flutter/material.dart';
+import '../widgets/item_widget.dart'; //'package:flutter_catalog/widgets/item_widget.dart';
+import '../models/catalog.dart';
+import '../widgets/drawer.dart'; 
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatelessWidget {
   final int days = 30;
   final String name = "par Camara";
 
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog App"),
       ),
-      body: Center(
-        child: Container (
-          child: Text("Welcome flutter $days days series $name"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
         ),
       ),
-      drawer: Drawer(),
+      drawer: MyDrawer(),
     );
-  }  
-}
-
-/**
- int days = 30;
-  String name = " par Camara";
-  double pi = 3.14;
-  bool isMale = true;
-  num temps = 30.5;
-
-  @override
-  Widget build(BuildContext context) {
-/*
-    return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container (
-          child: Text("Welcome flutter $days days series" + name),
-          ),
-        ),
-      ),
-    ); */
   }
-
- */
+}
